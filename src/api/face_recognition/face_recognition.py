e4cbc2c4858d0b3aa53222ca58cafe2b2577530e
Nguyen Ngoc Thien <nhoxpiin2306@gmail.com>
2023-03-31 19:52:23 +0700

    feat: create new user for recognition taask

--- src/api/face_recognition/face_recognition.py
+++ src/api/face_recognition/face_recognition.py
@@ -6,30 +6,27 @@ from deepface import DeepFace
 from typing import Union
 from fastapi.responses import JSONResponse
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join, dirname, isdir
 from core.database.connection import *
 from datetime import datetime
 import os
-import tensorflow as tf
-import tensorflow.keras as keras
 from typing import List
-import face_recognition as face
-from api.face_recognition.helper.face_helper import DistanceLayer, processing_images, read_image_user, Embedding
+import time
+from api.face_recognition.helper.face_helper import DistanceLayer, Embedding
 #endregion
 
 target = ""
-model_path = "/data/thinhlv/thiennn/deeplearning/insightface/recognition/arcface_torch/work_dirs/glint360k_cosface_r34_fp16_0.1/backbone.pth"
+model_path = "/data/thinhlv/thiennn/deeplearning/insightface/recognition/arcface_torch/work_dirs/glint360k_cosface_r100_fp16_0.1/backbone.pth"
 #model_path = "/data/thinhlv/thiennn/deeplearning/insightface/recognition/arcface_torch/work_dirs/ms1mv3_r50_onegpu/model.pt"
 image_path = "/data/thinhlv/thiennn/deeplearning/insightface/recognition/_datasets_/ms1m-retinaface-t1"
 result_dir = "/data/thinhlv/thiennn/deeplearning/insightface/recognition/arcface_torch/work_dirs/glint360k_cosface_r100_fp16_0.1"
-batch_size = 128
+batch_size = 32
 job = "pyface"
 use_norm_score = True  # if Ture, TestMode(N1)
 use_detector_score = True  # if Ture, TestMode(D1)
 use_flip_test = True  # if Ture, TestMode(F1)
-network = "r34"
+network = "r100"
 
 embedding = Embedding(model_path, (3, 112, 112), 128, network=network)
 
@@ -52,8 +49,7 @@ async def face_intr():
 @face_router.post('/face-extract')
 async def upload_image(plate_num: str, file: Union[UploadFile,None] = None):
     try:
-        print(plate_num)
-        print(file)
+        start_time = time.time()
         if plate_num == '' or plate_num == None:
             return JSONResponse(
             status_code = status.HTTP_400_BAD_REQUEST,
@@ -84,13 +80,18 @@ async def upload_image(plate_num: str, file: Union[UploadFile,None] = None):
         face_detected = DeepFace.detectFace(img_origin, detector_backend='ssd', enforce_detection=False)
         cv2.imwrite(img_detected, face_detected[:,:,::-1]*255)
         face_sevices = FaceServices(img_origin, img_detected, plate_num)
-        return face_sevices.create_track_vehicle(plate_num, img_detected, date_time, "", 0, 0)
-            
+        face_sevices.add_face(embedding.model, network, img_detected, FACE_DETECTED)
+        result = face_sevices.create_track_vehicle(plate_num, img_detected, date_time, "", 0, 0)
+        endtime = time.time()
+        print("the time excute of face_extract function is {}".format(endtime - start_time))
+        return result
+    
     except Exception as e:
         return JSONResponse(
             status_code = status.HTTP_400_BAD_REQUEST,
             content = { 'message' : str(e) }
             )
+
 
 @face_router.post('/face-verification')
 async def face_verification(plate_num: str, file: Union[UploadFile,None] = None):
@@ -125,7 +126,7 @@ async def face_verification(plate_num: str, file: Union[UploadFile,None] = None)
         
 @face_router.post("/face-recognition", description="")
 async def face_recognition(file: Union[UploadFile,None] = None):
-    import time
+
     try:
         start_time = time.time()
         if file == None:
@@ -139,7 +140,7 @@ async def face_recognition(file: Union[UploadFile,None] = None):
             content = await file.read()
             await out_file.write(content)
             
-        data = face_sevices.find_user(embedding.model, img_path, FACE_DETECTED, detector="ssd")
+        data = face_sevices.find_user(embedding.model, img_path, FACE_DETECTED, detector="ssd", model_name=network)
         print(data)
         end_time = time.time()
         return JSONResponse(
--- src/api/face_recognition/services/face_service.py
+++ src/api/face_recognition/services/face_service.py
@@ -39,7 +39,35 @@ class FaceServices:
                 content = { 'message' : str(e) }
             )
             
+    def add_face(self, model, model_name, img_path, db_path, silent=True):
+        file_name = f"{model_name}representations.pkl"
+        file_name = file_name.replace("-", "_").lower()
+        if os.path.exists(db_path + "/" + file_name):
+            if not silent:
+                print(
+                    f"WARNING: Representations for images in {db_path} folder were previously stored"
+                    + f" in {file_name}. If you added new instances after the creation, then please "
+                    + "delete this file and call find function again. It will create it again."
+                )
 
+            with open(f"{db_path}/{file_name}", "rb") as f:
+                representations = pickle.load(f)
+            
+            img = load_img(img_path, target_size=(112, 112))
+            img_representation = self.embedding_face(model, img)
+            print(f"add new user...")
+            instance = []
+            instance.append(img_path)
+            instance.append(img_representation)
+            instance.append(img_path.split("/")[-2])
+            representations.append(instance)
+                 
+            with open(f"{db_path}/{file_name}", "wb") as f:
+                pickle.dump(representations, f)
+            
+            if not silent:
+                print("There are ", len(representations), " representations found in ", file_name)
+    
     def embedding_face(self, model, img):
         #img = load_img(employee, target_size=(112, 112))
         img = img_to_array(img)
@@ -92,12 +120,12 @@ class FaceServices:
             },  
         }
         
-    def find_user(self, model, img_path, db_path, detector="opencv", enfore_detection=True, silent=False):
+    def find_user(self, model, img_path, db_path, model_name, detector="opencv", enfore_detection=True, silent=False):
         if os.path.isdir(db_path) is not True:
             raise ValueError("Passed db_path does not exist!")
         
         img_size = (112, 112)
-        file_name = f"representations.pkl"
+        file_name = f"{model_name}representations.pkl"
         file_name = file_name.replace("-", "_").lower()
         print(db_path + "/" + file_name)
         if os.path.exists(db_path + "/" + file_name):
@@ -143,18 +171,7 @@ class FaceServices:
             for index in pbar:
                 employee = employees[index]
                 img = load_img(employee, target_size=(112, 112))
-                img = img_to_array(img)
-                img_flip = cv2.flip(img, 1)
-                img_c = np.array([img, img_flip])
-                img_c = np.transpose(img_c, (0, 3, 1, 2))
-                img_1 = ((img_c[:1] / 255) - 0.5) / 0.5
-                img_2 = ((img_c[1:2] / 255) - 0.5) / 0.5
-                net_out_1 = model.cuda(0)(torch.from_numpy(img_1))
-                embedding1 = net_out_1.detach().cpu().numpy()
-                net_out_2 = model.cuda(0)(torch.from_numpy(img_2))
-                embedding2 = net_out_2.detach().cpu().numpy()
-                img_representation = sklearn.preprocessing.normalize(embedding1 + embedding2) 
-                
+                img_representation = self.embedding_face(model, img)  
                 instance = []
                 instance.append(employee)
                 instance.append(img_representation)
@@ -170,39 +187,27 @@ class FaceServices:
                     + "Please delete this file when you add new identities in your database."
                 )
                 
-        df = pd.DataFrame(representations, columns=["identity", "r100_representation", "plate_num"])
+        df = pd.DataFrame(representations, columns=["identity", f"{model_name}_representation", "plate_num"])
         
         img_face = DeepFace.detectFace(img_path, enforce_detection=False, detector_backend=detector, target_size=(112, 112))
         img_face = img_face[:,:,::-1]*255
         cv2.imwrite(img_path, img_face)
-        img = img_to_array(img_face)
-        print(img_face.shape)
-        print("--------")
-        print(img.shape)
-        img_flip = cv2.flip(img, 1)
-        img_c = np.array([img, img_flip])
-        img_c = np.transpose(img_c, (0, 3, 1, 2))
-        img_1 = ((img_c[:1] / 255) - 0.5) / 0.5
-        img_2 = ((img_c[1:2] / 255) - 0.5) / 0.5
-        net_out_1 = model.cuda(0)(torch.from_numpy(img_1))
-        embedding1 = net_out_1.detach().cpu().numpy()
-        net_out_2 = model.cuda(0)(torch.from_numpy(img_2))
-        embedding2 = net_out_2.detach().cpu().numpy()
-        print(embedding1.shape)
-        img_representation = sklearn.preprocessing.normalize(embedding1 + embedding2)
-        
+        #img = img_to_array(img_face)
+        img_representation = self.embedding_face(model, img_face)
+        print(img_representation.shape)
         distances = []
         print("finding user...")
         for index, instance in df.iterrows():
-            source_embedding = instance[f"r100_representation"]
+            source_embedding = instance[f"{model_name}_representation"]
             dist = euclid_distance(source_embedding, img_representation)
             distances.append(dist[0])
 
-        df["r100_euclid"] = distances
-        df = df.drop("r100_representation", axis=1)
-        df = df[df[f"r100_euclid"] <= 1.5]
+        df[f"{model_name}_euclid"] = distances
+        print(distances)
+        df = df.drop(f"{model_name}_representation", axis=1)
+        df = df[df[f"{model_name}_euclid"] <= 1.5]
         df = df.sort_values(
-            by=[f"r100_euclid"], ascending=True
+            by=[f"{model_name}_euclid"], ascending=True
         ).reset_index(drop=True)
         
         return df.head()    
