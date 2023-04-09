bc48e2c036ece4f2327c1d727a7adbabee8ba3df
Pham Van Manh Hung <hunghtk456@gmail.com>
2023-04-09 18:43:14 +0700

    Phat hien xe ra vao_Hung

---
+++ src/api/license_plates/plates_recognition.py
@@ -0,0 +1,97 @@
+from api.license_plates.service.ocrlp_service import ocr_service
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile, status, APIRouter, Response, Depends,WebSocket
+from typing import Union
+import cv2
+import numpy as np
+import base64
+import json
+import pickle
+from api.license_plates.model.BienSoXe.read_plate import PlatesServices
+import time
+recognition_router = APIRouter()
+service = ocr_service()
+#read_plate = PlatesServices()
+
+#get devices
+
+@recognition_router.post("/recognition_license")
+async def face_intr(file: Union[UploadFile,None] = None):
+    #return Response(status_code=200, content='Hello Test')
+    try:
+        if file == None:
+            return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = { 'message' : 'File can not null!' }
+            ) 
+        else:
+            img = cv2.imdecode(np.fromstring(file.file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+            #return read_plate.detection(img)
+            return None
+    except Exception as e:
+        return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = { 'message' : str(e) }
+            )
+@recognition_router.post("/license_detect")
+async def license_detect(file: Union[UploadFile,None] = None):
+    #return Response(status_code=200, content='Hello Test')
+    try:
+        if file == None:
+            return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = { 'message' : 'File can not null!' }
+            ) 
+        else:
+            start_time = time.time()
+            contents = await file.read()
+            # img = cv2.imdecode(np.fromstring(file.file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+            # print("Image",img)
+            image = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR)
+            listChar,typelp,typevehi = service.ocr(image)
+            print("Chuoi bien so", listChar)
+            end_time = time.time()
+            print(f"Thời gian thực thi: {end_time - start_time} giây")
+            return JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content = { 'message' : {"Characters":listChar, "TypeLP": typelp, "Vehicle":typevehi }  }
+            ) 
+            
+    except Exception as e:
+        return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = { 'message' : str(e) }
+            )
+    
+@recognition_router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    try:
+        await websocket.accept()
+        while True:
+                print("websocket")
+                # Nhận dữ liệu từ client
+                data = await websocket.receive_text()
+                
+                # Parse dữ liệu nhận được thành đối tượng JSON
+                json_data = json.loads(data)
+                # Lấy dữ liệu ảnh từ trường "Image"
+                img_str = base64.b64decode(json_data['Image'])
+                # Chuyển đổi dữ liệu ảnh thành đối tượng numpy array
+                image_np = cv2.imdecode(np.frombuffer(img_str, np.uint8), cv2.IMREAD_COLOR)
+                
+                print(image_np.shape)
+            
+                predictions =json_data['Predictions']
+                if len(predictions) != 0: 
+                    listChar,typelp,typevehi = service.ocrYolo(image_np,predictions)
+                    print("Chuoi bien so", listChar)
+                # Xử lý dữ liệu nhận được tại đây
+                # Gửi phản hồi về cho client
+                await websocket.send_text(f"Server received data")
+    except Exception as e:
+        print('message' ,str(e))
+        return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = { 'message' : str(e) }
+            )
--- src/api/license_plates/service/ocrlp_service.py
+++ src/api/license_plates/service/ocrlp_service.py
@@ -1,5 +1,7 @@
 from api.license_plates.model.detectRoboflow.loadRoboflow import RoboflowModel
 from api.license_plates.characters_recognition.characters_recognition import CharactersRecognition
+import cv2
+import pytesseract
 from api.license_plates.characters_recognition.helper.imageprocessing import processing
 roboflow = RoboflowModel("66nm8eEysyrDBVqqkTTV","license-plates-recognition-iuk6u")
 cRecognition = CharactersRecognition()
@@ -12,7 +14,7 @@ class ocr_service:
         x_max = x_min + w
         y_max = y_min + h
         # Crop the image
-        cropped_img = image[y_min:y_max, x_min:x_max]
+        cropped_img = image[y_min:y_max, x_min:x_max] 
         return cropped_img
     def ocr(self,image):
         kitu,typelp,typevehicle = roboflow.detect_objects(image)
@@ -20,13 +22,35 @@ class ocr_service:
         characters = []
 
         for object in kitu:
+            
             cropped_img = self.crop_image(image, object[0], object[1], object[2], object[3])
             ## Chuyen doi anh xam
             imgProcessing=processing(cropped_img)
             # đưa vào nhận diện  .............. 
 
             result_test= cRecognition.loadimg_Test(imgProcessing)
+            #result_test = pytesseract.image_to_string(imgProcessing)
             if(False): # Cài đặt ngưỡng kết quả
+                continue
+            print("result_test", result_test.item())
+            print("result_test", result_test)
+            character= self.lables[result_test.item()]
+            characters+= character
+        # Concat the characters
+        result = "".join(characters)
+        return result,typelp,typevehicle
+    # def crop_imageyolo(self, image, x_min, y_min, w, h):
+
+    def ocrYolo(self,image,predictions):
+        kitu,typelp,typevehicle = roboflow.detect_objectYolo(predictions)
+        characters = []
+        i = 0
+        for object in kitu:
+            cropped_img = self.crop_image(image,int(object[0]), int(object[1]), int(object[2]), int(object[3]))
+            ## Convert Processing image
+            imgProcessing=processing(cropped_img)
+            result_test= cRecognition.loadimg_Test(imgProcessing)
+            if(False): # Settting threshold
                 continue
             print("result_test", result_test.item())
             print("result_test", result_test)
